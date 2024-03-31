03ce7de12175976fd4270966bbca3195c5bbda37
Dinuk1994 <ddt94119@gmail.com>
2024-03-31 12:49:26 +0530

    Initializing Standalone weather App

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,18 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+import SearchBar from './Search-bar/SearchBar'
+import '../src/App.css/App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='SearchBar'>
+        <SearchBar />
+      </div>
+    </div>
+
+  )
+}
+
+export default App
+
